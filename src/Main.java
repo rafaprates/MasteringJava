ea365ff7acf5459f222cdcc9d9e3cad478a7a52b
rafaprates <rafaelbprates@gmail.com>
2022-08-21 17:28:32 -0400

    [add] exercise 39

--- src/Main.java
+++ src/Main.java
@@ -2,18 +2,34 @@ import Section_6_and_7_OOP.CarpetCostCalculator.Calculator;
 import Section_6_and_7_OOP.CarpetCostCalculator.Carpet;
 import Section_6_and_7_OOP.CarpetCostCalculator.Floor;
 import Section_6_and_7_OOP.Encapsulation.Printer;
+import Section_6_and_7_OOP.Polymorfism.Car;
+import Section_6_and_7_OOP.Polymorfism.Ford;
+import Section_6_and_7_OOP.Polymorfism.Holden;
+import Section_6_and_7_OOP.Polymorfism.Mitsubishi;
 
 class Main {
 
     public static void main(String[] args) {
+        Car car = new Car(8, "Base car");
+        System.out.println(car.startEngine());
+        System.out.println(car.accelerate());
+        System.out.println(car.brake());
 
-        Printer printer = new Printer(50, true);
-        System.out.println(printer.addToner(50));
-        System.out.println("initial page count = " +printer.getPagesPrinted());
-        int pagesPrinted = printer.printPages(4);
-        System.out.println("Pages printed was " + pagesPrinted +" new total print count for printer = " +printer.getPagesPrinted());
-        pagesPrinted = printer.printPages(2);
-        System.out.println("Pages printed was " + pagesPrinted +" new total print count for printer = " +printer.getPagesPrinted());
+        Mitsubishi mitsubishi = new Mitsubishi(6, "Outlander VRX 4WD");
+        System.out.println(mitsubishi.startEngine());
+        System.out.println(mitsubishi.accelerate());
+        System.out.println(mitsubishi.brake());
+
+        Ford ford = new Ford(6, "Ford Falcon");
+        System.out.println(ford.startEngine());
+        System.out.println(ford.accelerate());
+        System.out.println(ford.brake());
+
+        Holden holden = new Holden(6, "Holden Commodore");
+        System.out.println(holden.startEngine());
+        System.out.println(holden.accelerate());
+        System.out.println(holden.brake());
+
     }
 
 }
