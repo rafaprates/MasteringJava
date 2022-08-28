6cec6bb00cf3a26bbc6277ebaf0ec9900684cb67
rafaprates <rafaelbprates@gmail.com>
2022-08-28 13:19:14 -0400

    [update] exercise 46 code clean up

--- src/Section_8_Arrays_Lists_Autoboxing_Unboxing/Playlist/Album.java
+++ src/Section_8_Arrays_Lists_Autoboxing_Unboxing/Playlist/Album.java
@@ -37,23 +37,16 @@ public class Album {
     }
 
     public boolean addToPlayList(int albumTrackNumber, LinkedList<Song> playList) {
-//        System.out.println("Album " + name + " is " + songs.size()  + " songs long.");
-//        System.out.println("Track number is " + albumTrackNumber);
-//        System.out.println("Get song at index " + (albumTrackNumber - 1));
-//        System.out.println("\n");
 
         if (albumTrackNumber < 1 || albumTrackNumber > songs.size()) {
-//            System.out.println("trackNumber is out of bounds");
             return false;
         }
 
-//        System.out.println(songs.get(albumTrackNumber-1).getTitle());
         Song songToAdd = songs.get(albumTrackNumber - 1);
         if(findSong(songToAdd.getTitle()) != null) {
             playList.add(songToAdd);
             return true;
         }
-//        System.out.println("Song does not exist.");
         return false;
     }
 
@@ -62,7 +55,6 @@ public class Album {
         if (songToAdd != null) {
             return playlist.add(songToAdd);
         }
-//        System.out.println("Song " + songName + " does not exist.");
         return false;
     }
 }
